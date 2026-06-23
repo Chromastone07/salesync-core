648f0c40ae115309b5fc38bd4832735f5e3742c7
chromastone07 <okappeal@gmail.com>
2026-06-23 08:38:02 +0530

    Fix deployment bugs

--- apps/dukaan/metro.config.js
+++ apps/dukaan/metro.config.js
@@ -1,3 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const path = require('path');
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
